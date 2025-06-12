81043671ccc16b287d2cc66f57e7585affe09867
Varun Nayaka G <varunnayaka248@gmail.com>
2025-06-12 12:21:21 +0530

    Delete src directory

--- src/hooks/use-toast.ts
+++
@@ -1,7 +0,0 @@
-
-import { useToast as useShadcnToast } from "@/components/ui/use-toast";
-import { toast as shadcnToast } from "@/components/ui/toast";
-
-export const useToast = useShadcnToast;
-
-export const toast = shadcnToast;
